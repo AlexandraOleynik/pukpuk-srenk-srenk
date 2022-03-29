f8fce32ee6188e52b46feee21abbea9353509c10
AlexandraOleynik <99476697+AlexandraOleynik@users.noreply.github.com>
2022-03-29 21:07:43 +0300

    Add files via upload

---
+++ stillwannakmslf/javascript/java-script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$("#square2").click(function(){
+    $("#start1").css("display", "none");
+    $("#screen2").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#arrow1").click(function(){
+    $("#screen2").css("display", "none");
+    $("#start1").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#square1").click(function(){
+    $("#start1").css("display", "none");
+    $("#game1").css("display", "block");
+});
+});
